55524952e138effe20b689529884269530f00be4
ModyQyW <wurui-dev@foxmail.com>
2023-01-06 17:30:00 +0800

    chore: update sponsors

--- custom-sponsors.ts
+++ custom-sponsors.ts
@@ -1,5 +1,21 @@
 import { Sponsorship } from "sponsorkit";
 
-const sponsors: Sponsorship[] = [];
+const sponsors: Sponsorship[] = [
+  {
+    sponsor: {
+      type: "User",
+      login: "wangzecn",
+      name: "王泽",
+      avatarUrl: "https://avatars.githubusercontent.com/u/35653404",
+      linkUrl: "https://github.com/wangzecn",
+    },
+    monthlyDollars: 1.25,
+    privacyLevel: "PUBLIC",
+    tierName: "Wechat",
+    createdAt: "2023-01-06T17:22:01+08:00",
+    isOneTime: true,
+    provider: "Wechat",
+  },
+];
 
 export default sponsors;
